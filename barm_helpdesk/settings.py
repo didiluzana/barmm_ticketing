daa29c6ba9457c36ad450bc79b0de93ae9d40596
Jijiluzana225 <jiji.luzana@gmail.com>
2026-09-22 01:42:47 +0800

    SSASD

--- barm_helpdesk/settings.py
+++ barm_helpdesk/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "change-me-in-production")
 
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
