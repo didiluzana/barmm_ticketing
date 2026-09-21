3708ab3e757c506ca4b8045a61725bfa801a539c
Didi <didi.luzana@gmail.com>
2026-09-22 03:38:06 +0800

    changepass import

--- tickets/views.py
+++ tickets/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
 from django.db.models import Q
 from django.shortcuts import (
     get_object_or_404,
