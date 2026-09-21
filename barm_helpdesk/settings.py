1c7e1ce52c290369205a774b00e01442c61f8c45
Didi <didi.luzana@gmail.com>
2026-09-22 02:30:19 +0800

    Change database password and host in settings.py
    Updated database settings to use sadfadfpecific password and host.

--- barm_helpdesk/settings.py
+++ barm_helpdesk/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("PGDATABASE", "railway"),
         "USER": os.environ.get("PGUSER", "postgres"),
-        "PASSWORD": os.environ.get("PGPASSWORD", ""),
-        "HOST": os.environ.get("PGHOST", ""),
+        "PASSWORD": os.environ.get("PGPASSWORD", "JxJWGyiELjnWiQZCMUOioSYjfDWztRZR"),
+        "HOST": os.environ.get("PGHOST", "postgres.railway.internal"),
         "PORT": os.environ.get("PGPORT", "5432"),
     }
 }
